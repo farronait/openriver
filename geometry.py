1f54bf046f4afa7dcd0915c98154f312aa4088df
Pietro Zambelli <pietro.zambelli@ing.unitn.it>
2010-03-22 15:28:00 +0100

    Add doctest in geometry.py

--- geometry.py
+++ geometry.py
@@ -56,18 +56,40 @@ class Section:
         self.segment.append(segment)
 
     def firstPointAfter_h(self, points, h):
-        #print 'points:', points,  h
-        for i, p in enumerate(points):
-            print i,  p
-            if p > h:
-                return i
+        """Return index of the first
+
+        >>> points=np.array([ 742.73,  742.75,  742.77,  742.79,  747.27])
+
+        >>> section.firstPointAfter_h(points, 745)
+        4
+
+        >>> section.firstPointAfter_h(points, 742)
+        Traceback (innermost last):
+          ...
+        ValueError: h outside section
+         h < min
+
+        >>> section.firstPointAfter_h(points, 748)
+        Traceback (innermost last):
+          ...
+        ValueError: h outside section
+         h > max
+        """
+        if h > points.max():
+            raise ValueError("h outside section\n h > max")
+        elif h < points.min():
+            raise ValueError("h outside section\n h < min")
+        else:
+            #print 'points:', points,  h
+            for i, p in enumerate(points):
+                #print i,  p
+                if p > h:
+                    return i
 
     def intersection(self, pn1, pn2, h):
         """Returnurn intersection between 2 points and height
 
-        Examples: ::
-
-            >>> sct.intersection((0,5),(0,0),3)
+            >>> section.intersection((0,5),(0,0),3)
             (0, 3)
 
         (h-y0)/(y1-y0) = (x-x0)/(x1-x0)
@@ -78,7 +100,18 @@ class Section:
         return (pn2[0]-pn1[0])/(pn2[1]-pn1[1])*(h-pn1[1])+pn1[0], h
 
     def getSect(self, h):
-        """Return section only from left intersection to right intersection"""
+        """Return section only from left intersection to right intersection
+
+            >>> section.getSect(745)
+            array([[  4.71227679e-01,   7.45000000e+02],
+                   [  9.30000000e-01,   7.42790000e+02],
+                   [  7.19000000e+00,   7.42770000e+02],
+                   [  1.25900000e+01,   7.42750000e+02],
+                   [  1.80800000e+01,   7.42730000e+02],
+                   [  1.89100000e+01,   7.42730000e+02],
+                   [  1.94887253e+01,   7.45000000e+02]])
+
+        """
         coordtrasp=self.coord.T
         lefttomin=self.coord[1][:self.min+1]
         # left point
@@ -99,11 +132,15 @@ class Section:
         sez=np.insert(sez, [0,], l_intersect,axis=0)
         # Add rightht intersection on the bottom
         sez=np.append(sez,[r_intersect],axis=0)
-        #print "sez finale:\n", sez
         return sez
 
     def area(self, sez):
-        """Return area given a section take from getSect"""
+        """Return area given a section take from getSect
+
+            >>> sez=section.getSect(745)
+            >>> section.area(sez)
+            41.448496601989973
+        """
         # find area below water line
         area_h2o = (sez[-1][0]-sez[0][0])*sez[0][1]
         # find area bellow section
@@ -111,24 +148,22 @@ class Section:
         return area_h2o - area_sez
 
     def wetBorder(self,  sez):
-        """Calculate web border from a given section"""
+        """Calculate web border from a given section
+
+            >>> sez=section.getSect(745)
+            >>> section.wetBorder(sez)
+            23.557497596153816
+        """
         # calculate with pitagora: sqrt(dx²+dy²)
         sez1=np.delete(sez, 0, axis=0)
         sez2=np.delete(sez, -1, axis=0)
         delta=sez1-sez2
-        print "\ndelta: \n", delta
         return np.sum(np.sqrt(delta * delta))
 
     def rh(self, h):
         """Return thee idraulic radius given height
 
-        Examples: ::
-
-            >>> sezdata52 = array([
-            [   0.  ,    0.93,    7.19,   12.59,   18.08,   18.91,   20.07],
-            [ 747.27,  742.79,  742.77,  742.75,  742.73,  742.73,  747.28]])
-            >>> sct=Section(yzcoord=sezdata)
-            >>> sct.rh(745)
+            >>> section.rh(745)
             1.7594609288533762
         """
         sez=self.getSect(h)
@@ -186,4 +221,10 @@ class Reach:
         print("ok")
 
 
+if __name__ == "__main__":
+    import doctest
+    sezdata=np.array([[0.000, 0.930,  7.190, 12.590, 18.080, 18.910, 20.070],
+[747.27000, 742.79000, 742.77000, 742.75000, 742.73000, 742.73000, 747.28000]])
+    section=Section(yzcoord=sezdata)
+    doctest.testmod()
 
